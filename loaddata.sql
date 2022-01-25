8fa88cd7ee0bb1fb2521f89698e2b2626ba0cf16
Sherwin Vargas <svargas025@gmail.com>
2022-01-25 13:45:00 -0600

    user posts and post detais

--- loaddata.sql
+++ loaddata.sql
@@ -87,3 +87,4 @@ CREATE TABLE "Categories" (
 INSERT INTO Categories ('label') VALUES ('News');
 INSERT INTO Tags ('label') VALUES ('JavaScript');
 INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
+INSERT INTO Posts ("user_id", "category_id", "title", "publication_date", "image_url", "content", "approved") VALUES (1, 1, "works?", 01102022, "google.com", "ordered?", 1)
--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+from views.posts import create_post, delete_post, get_all_posts, get_single_post, update_post
 
 from views.user import create_user, login_user
 
@@ -50,8 +51,20 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        """Handle Get requests to the server"""
-        pass
+        self._set_headers(200)
+        response = {}
+        
+        parsed = self.parse_url()
+        
+        if len(parsed) == 2:
+            (resource, id) = parsed
+            if resource == "posts":
+                if id is not None: 
+                    response = get_single_post(id)
+                else: 
+                    response = get_all_posts()
+                    
+        self.wfile.write(response.encode())
 
 
     def do_POST(self):
@@ -66,17 +79,41 @@ class HandleRequests(BaseHTTPRequestHandler):
             response = login_user(post_body)
         if resource == 'register':
             response = create_user(post_body)
+        if resource == "posts":
+            response = create_post(post_body)
 
         self.wfile.write(response.encode())
+        
+        
 
     def do_PUT(self):
-        """Handles PUT requests to the server"""
-        pass
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+        
+        (resource, id) = self.parse_url()
+        
+        success = False
+        
+        if resource == "posts":
+            success = update_post(id, post_body)
+            
+        if success:
+            self._set_headers(204)
+        else: 
+            self._set_headers(404)
+        
+        self.wfile.write("".encode()) 
 
     def do_DELETE(self):
-        """Handle DELETE Requests"""
-        pass
+        self._set_headers(204)
+        
+        (resource, id) = self.parse_url()
+        
+        if resource == "posts":
+            delete_post(id)
 
+        self.wfile.write("".encode())
 
 def main():
     """Starts the server on port 8088 using the HandleRequests class
